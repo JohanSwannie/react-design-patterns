246b147433f8299004c553b00016190eebb46f63
jswan <jswannieman@gmail.com>
2023-02-28 09:14:21 +1100

    Update index JS Component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import SplitScreenApp from "./SplitscreenApp";
+import Lists from "./Lists";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <SplitScreenApp />
+    <Lists />
   </React.StrictMode>
 );
