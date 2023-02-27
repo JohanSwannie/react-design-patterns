7d111fb32633cd6c896bcae98a1ec3ec799c4816
jswan <jswannieman@gmail.com>
2023-02-28 09:14:46 +1100

    Update Lists Javascript component

---
+++ src/Lists.js
@@ -0,0 +1,95 @@
+// Lists and List Items
+
+import RegularList from "./RegularList";
+import NumberedList from "./NumberedList";
+import SmallPersonListItem from "./people/SmallPersonListItem";
+import LargePersonListItem from "./people/LargePersonListItem";
+import SmallProductListItem from "./products/SmallProductListItem";
+import LargeProductListItem from "./products/LargeProductListItem";
+
+const people = [
+  {
+    name: "Tommie Theron",
+    age: 31,
+    haircolor: "brown",
+    hobbies: ["golf", "gym", "reading", "horse riding"],
+  },
+  {
+    name: "Luke Matthew",
+    age: 28,
+    haircolor: "blond",
+    hobbies: ["table tennis", "walking", "swimming", "fishing"],
+  },
+  {
+    name: "Mary Brown",
+    age: 29,
+    haircolor: "black",
+    hobbies: ["reading", "stamp collecting", "running", "walking"],
+  },
+  {
+    name: "Mark Cooper",
+    age: 22,
+    haircolor: "brown",
+    hobbies: ["gym", "fishing", "cycling", "running"],
+  },
+];
+
+const products = [
+  {
+    name: "Sandals",
+    description: "100% leather Sandals",
+    price: 74.99,
+    rating: 9.2,
+  },
+  {
+    name: "Running shoes",
+    description: "Best ever running shoes",
+    price: 210.0,
+    rating: 9.3,
+  },
+  {
+    name: "Slippers",
+    description: "Comfortable slippers",
+    price: 29.55,
+    rating: 8.4,
+  },
+  {
+    name: "Fishing shoes",
+    description: "Best practical fishing shoes",
+    price: 48.95,
+    rating: 9.0,
+  },
+];
+
+function Lists() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1 style={{ textDecoration: "underline" }}>Small Person List Item</h1>
+      <RegularList
+        items={people}
+        resourceName="person"
+        itemComponent={SmallPersonListItem}
+      />
+      <h1 style={{ textDecoration: "underline" }}>Large Person List Item</h1>
+      <NumberedList
+        items={people}
+        resourceName="person"
+        itemComponent={LargePersonListItem}
+      />
+      <h1 style={{ textDecoration: "underline" }}>Small Product List Item</h1>
+      <RegularList
+        items={products}
+        resourceName="product"
+        itemComponent={SmallProductListItem}
+      />
+      <h1 style={{ textDecoration: "underline" }}>Large Product List Item</h1>
+      <NumberedList
+        items={products}
+        resourceName="product"
+        itemComponent={LargeProductListItem}
+      />
+    </div>
+  );
+}
+
+export default Lists;
