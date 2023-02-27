2a029178395be960e9b2de53ecc8aa9a864e6368
jswan <jswannieman@gmail.com>
2023-02-28 09:58:29 +1100

    Add ModalsApp Javascript component

---
+++ src/ModalApp.js
@@ -0,0 +1,24 @@
+import Modal from "./Modal";
+
+const ModalApp = () => {
+  return (
+    <>
+      <Modal>
+        <ul>
+          <li>This is a Modal for Display purposes</li>
+          <li>This is a Modal for Display purposes</li>
+          <li>This is a Modal for Display purposes</li>
+          <li>This is a Modal for Display purposes</li>
+          <li>This is a Modal for Display purposes</li>
+          <li>This is a Modal for Display purposes</li>
+          <li>This is a Modal for Display purposes</li>
+          <li>This is a Modal for Display purposes</li>
+          <li>This is a Modal for Display purposes</li>
+          <li>This is a Modal for Display purposes</li>
+        </ul>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalApp;
