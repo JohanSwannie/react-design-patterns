ab5961523e0f4eae68362083a93a6d1e835e9db2
jswan <jswannieman@gmail.com>
2023-02-28 09:08:34 +1100

    Update SplitScreenApp JS Component

---
+++ src/SplitscreenApp.js
@@ -0,0 +1,48 @@
+// Creating Split Screens
+
+import styled from "styled-components";
+import { SplitScreen } from "./SplitScreen";
+
+const LeftStyle = styled.h1`
+  text-align: center;
+  background-color: green;
+  height: 95vh;
+`;
+
+const MiddleStyle = styled.h1`
+  text-align: center;
+  color: #fff;
+  font-weight: bold;
+  background-color: navy;
+  height: 95vh;
+`;
+
+const RightStyle = styled.h1`
+  text-align: center;
+  background-color: orange;
+  height: 95vh;
+`;
+
+const LeftHandComponent = ({ leftHandText }) => {
+  return <LeftStyle>{leftHandText}</LeftStyle>;
+};
+
+const MiddleComponent = ({ middleText }) => {
+  return <MiddleStyle>{middleText}</MiddleStyle>;
+};
+
+const RightHandComponent = ({ rightHandText }) => {
+  return <RightStyle>{rightHandText}</RightStyle>;
+};
+
+function SplitScreenApp() {
+  return (
+    <SplitScreen leftWeight={1} middleWeight={2} rightWeight={3}>
+      <LeftHandComponent leftHandText="Left Pane" />
+      <MiddleComponent middleText="Middle Pane" />
+      <RightHandComponent rightHandText="Right Pane" />
+    </SplitScreen>
+  );
+}
+
+export default SplitScreenApp;
