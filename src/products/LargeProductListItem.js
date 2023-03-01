eee5d6e7ba67760750648c5ecfb6061eee2cf6a4
jswan <jswannieman@gmail.com>
2023-03-01 13:54:32 +1100

    Update LaregProductListItem JS Component

--- src/products/LargeProductListItem.js
+++ src/products/LargeProductListItem.js
@@ -2,7 +2,7 @@ const LargeProductListItem = ({ product }) => {
   const { name, description, price, rating } = product;
   return (
     <>
-      <h3>{name}</h3>
+      <h4>{name}</h4>
       <p>{price}</p>
       <h3>Description:</h3>
       <p>{description}</p>
